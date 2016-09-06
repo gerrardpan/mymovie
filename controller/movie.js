0d7c978aa574154cfc4bb9fd829d4f16bb7451b1
gerrard_pan <gerrard_pan@yahoo.com>
2016-09-06 23:01:06 +0800

    ..

--- controller/movie.js
+++ controller/movie.js
@@ -6,12 +6,13 @@ var movie = models.movie;
 
 exports.addmovie = function(req,res){
 
-       res.reder("addMovie.jade");
+       res.render("addMovie.jade");
     };
 
 exports.details = function(req,res){
 
 	    movie.findOne({mName:"Capteam America"},function(err,doc){
+	    	
 		console.log(req.session.user+"read session");
 	    res.render("details.jade",doc);
 
